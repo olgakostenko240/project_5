5de696161d5ef513105d32d83c777d5a79dffc8b
Ольга Костенко <you@example.com>
2025-04-19 23:10:06 +0500

    Созданы модели well и lesson с необходимыми полями

---
+++ materials/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+
+class Well(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название курса", help_text="Укажите название курса"
+    )
+    preview = models.ImageField(
+        upload_to="materials/previews",
+        blank=True,
+        null=True,
+        verbose_name="Картинка",
+        help_text="Загрузите картинку",
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса",
+        help_text="Укажите описание курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=50, verbose_name="Название урока", help_text="Укажите название урока"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока",
+        help_text="Укажите описание урока",
+    )
+    well = models.ForeignKey(
+        Well,
+        on_delete=models.SET_NULL,
+        verbose_name="Курс",
+        help_text="Выберите курс",
+        blank=True,
+        null=True,
+    )
+    preview = models.ImageField(
+        upload_to="materials/images",
+        blank=True,
+        null=True,
+        verbose_name="Картинка",
+        help_text="Загрузите картинку",
+    )
+    link_to_video = models.URLField(
+        max_length=150,
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
