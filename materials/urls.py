96dcb3cef36bad8fdf919cfcfd2c4b0fb790a6e5
Ольга Костенко <you@example.com>
2025-04-19 23:23:54 +0500

    Прописаны пути для контроллеров

---
+++ materials/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from materials.apps import MaterialsConfig
+from materials.views import (LessonCreateApiView, LessonDestroyApiView,
+                             LessonListApiView, LessonRetrieveApiView,
+                             LessonUpdateApiView, WellViewSet)
+
+app_name = MaterialsConfig.name
+
+router = SimpleRouter()
+router.register("", WellViewSet)
+
+urlpatterns = [
+    path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
+    path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
+    path(
+        "lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"
+    ),
+    path(
+        "lessons/<int:pk>/delete/",
+        LessonDestroyApiView.as_view(),
+        name="lessons_delete",
+    ),
+]
+
+urlpatterns += router.urls
