48a2acf87897ddc87020ecc5ca1877e340893966
Ольга Костенко <you@example.com>
2025-04-26 22:43:48 +0500

    Прописаны пути для токенов

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from users.apps import UsersConfig
 from users.views import UserCreateApiView, PaymentViewSet, UserListApiView
@@ -13,6 +18,9 @@ router.register("payment", PaymentViewSet)
 urlpatterns = [
     path("user/", UserListApiView.as_view(), name="user_list"),
     path("user/create/", UserCreateApiView.as_view(), name="user_create"),
+
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
