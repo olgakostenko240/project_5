b6ee575befac91ecefff36cf50e18693e9132e4c
Ольга Костенко <you@example.com>
2025-05-08 22:52:24 +0500

    Написан контроллер для сериалайзера подписки

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
@@ -7,15 +8,18 @@ from rest_framework.generics import (
 )
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from materials.models import Lesson, Well
-from materials.serializers import LessonSerializer, WellSerializer, WellDetailSerializer
+from materials.models import Lesson, Well, Subscription
+from materials.serializers import LessonSerializer, WellSerializer, WellDetailSerializer, SubscriptionSerializer
+from materials.paginations import CustomPagination
 from users.permissions import IsModer, IsOwner
 
 
 class WellViewSet(ModelViewSet):
     queryset = Well.objects.all()
     serializer_class = WellSerializer
+    pagination_class = CustomPagination
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -51,6 +55,7 @@ class LessonCreateApiView(CreateAPIView):
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    pagination_class = CustomPagination
 
 
 class LessonRetrieveApiView(RetrieveAPIView):
@@ -69,3 +74,23 @@ class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, ~IsModer | IsOwner,)
+
+
+class SubscriptionCreateApiView(CreateAPIView):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        user = self.request.user
+        well_id = self.request.data.get('well')
+        well_item = get_object_or_404(Well, pk=well_id)
+        subs_item = Subscription.objects.filter(user=user, well=well_item)
+
+        if subs_item.exists():
+            subs_item.delete()  # Удаляем подписку
+            message = 'подписка удалена'
+        else:
+            Subscription.objects.create(user=user, well=well_item, sign_of_subscription=True)  # Создаем подписку
+            message = 'подписка добавлена'
+        return Response({"message": message})
